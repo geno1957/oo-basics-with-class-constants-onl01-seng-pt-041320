2a25f8bec75d36d893931d253e94d59b22108783
Jared  E <jared.epps322@gmail.com>
2020-04-29 18:31:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,9 +7,10 @@ class Shoe
   
 
   def initialize(brand)
-    @brand = brand
-   BRANDS << brand
-   BRANDS.uniq
+   @brand = brand
+   if BRANDS.include?(brand)
+     BRANDS << brand
+   end 
   end
 
   def cobble
